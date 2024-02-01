92a8006c5be5b2cdf2efb60110232c78d69c986a
VladDryazgov <vlad1d0@outlook.com>
2024-02-01 11:43:03 +0300

    More lines

--- new_test_file.cpp
+++ new_test_file.cpp
@@ -3,4 +3,11 @@
 int main()
 {
     std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    
 }
